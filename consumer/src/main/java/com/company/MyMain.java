e7a4af9e16828d69c025ac6c99d3aff31e27d145
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-12-28 12:23:59 -0700

    first commit

---
+++ consumer/src/main/java/com/company/MyMain.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.company.MyUtil;
+
+public class MyMain {
+    
+    public static void main(String args[]) {
+        System.out.println(MyUtil.add(1, 2));
+    }
+}
---
+++ producer/src/main/java/com/company/MyUtil.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public final class MyUtil {
+    
+    private MyUtil() {}
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
